027f7e00723c38f7cf2758c2560080bdc928549b
kartik <kartikkumar2778@gmail.com>
2024-11-13 22:27:32 +0530

    api change updated

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'https://serene-joy-production.up.railway.app/',
+  baseURL: 'https://serene-joy-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${localStorage.getItem('token') || ''}`, // Optional: Add default authorization header
