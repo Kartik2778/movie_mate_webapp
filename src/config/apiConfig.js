e0c34fccd88aea33627b7cc2dae3a67d69289cad
kartik <kartikkumar2778@gmail.com>
2024-11-14 11:51:40 +0530

    backend url changed

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://serene-joy-production.up.railway.app',
+  baseURL: 'https://movie-mate-webapp-bakcend-production.up.railway.app',
 });
 
 axiosInstance.interceptors.request.use((config) => {
